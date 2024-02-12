7eb83e4a5b9c899295808cb85d8ade0d7b47981c
Phillip Jensen <phillip@golemgrid.com>
2024-02-12 12:47:01 +0100

    Add posthog to capture sessions

---
+++ components/analytics/Banner.jsx
@@ -0,0 +1,38 @@
+const AnalyticsBanner = ({ setPreviousConsent, setAskedForConsent, posthog }) => {
+    return (
+        <div className="fixed bottom-0 left-0 right-0 bg-golemblue text-white z-50 border-t border-gray-700">
+            <div className="mx-auto max-w-7xl px-6 gap-8 py-3 items-center lg:flex lg:px-8">
+                <div>
+                    Golem Stats uses cookies to improve the user experience. Cookies are optional but its recommended and helps us improve
+                    the experience
+                </div>
+                <div className="flex gap-4 mt-4 lg:mt-0">
+                    <button
+                        className="text-white hover:text-white/90"
+                        onClick={() => {
+                            localStorage.setItem("GolemStatsAnalyticsConsent", "false")
+                            setPreviousConsent(false)
+                            setAskedForConsent(true)
+                            posthog.opt_out_capturing()
+                        }}
+                    >
+                        Decline
+                    </button>
+                    <button
+                        className="rounded-sm border border-white bg-primary px-4 py-1 text-white hover:bg-white/10"
+                        onClick={() => {
+                            localStorage.setItem("GolemStatsAnalyticsConsent", "true")
+                            setPreviousConsent(true)
+                            setAskedForConsent(true)
+                            posthog.opt_in_capturing()
+                        }}
+                    >
+                        Accept
+                    </button>
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default AnalyticsBanner
--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,26 +1,71 @@
-import { Navbar } from "@/components/Navbar"
 import "@/styles/globals.css"
-import type { AppProps } from "next/app"
-import NextNProgress from "nextjs-progressbar"
-import Script from "next/script"
-import { GoogleAnalytics } from "nextjs-google-analytics"
-import Banner from "@/components/Banner"
 import { SessionProvider } from "next-auth/react"
+import NextNProgress from "nextjs-progressbar"
+import { Navbar } from "@/components/Navbar"
+import Banner from "@/components/Banner"
+import type { AppProps } from "next/app"
+import AnalyticsBanner from "@/components/analytics/Banner"
+import { useEffect, useState } from "react"
+import { useRouter } from "next/router"
+import posthog from "posthog-js"
+import { PostHogProvider } from "posthog-js/react"
+import { GoogleAnalytics } from "nextjs-google-analytics"
+
+// Check that PostHog is client-side (used to handle Next.js SSR)
+if (typeof window !== "undefined") {
+    posthog.init(process.env.NEXT_PUBLIC_POSTHOG_KEY!, {
+        api_host: process.env.NEXT_PUBLIC_POSTHOG_HOST,
+        capture_pageview: false, // Disable automatic pageview capture, as we capture manually
+        autocapture: false,
+        loaded: (posthog) => {
+            if (process.env.NODE_ENV === "development") posthog.debug()
+        },
+    })
+}
 
 export default function App({ Component, pageProps: { session, ...pageProps } }: AppProps) {
+    const clientrouter = useRouter()
+    const [previousConsent, setPreviousConsent] = useState(false)
+    const [askedForConsent, setAskedForConsent] = useState(true)
+
+    useEffect(() => {
+        const isAnalyticsEnabled = localStorage.getItem("GolemStatsAnalyticsConsent")
+
+        if (isAnalyticsEnabled === null) {
+            setAskedForConsent(false)
+        }
+
+        if (isAnalyticsEnabled === "true") {
+            setPreviousConsent(true)
+            const handleRouteChange = () => posthog.capture("$pageview")
+            clientrouter.events.on("routeChangeComplete", handleRouteChange)
+
+            return () => {
+                clientrouter.events.off("routeChangeComplete", handleRouteChange)
+            }
+        } else {
+            setPreviousConsent(false)
+        }
+    }, [])
     return (
         <>
-            <SessionProvider session={session}>
-                <Navbar />
-                <NextNProgress color="#ffffff" />
+            {!askedForConsent ? (
+                <>
+                    <AnalyticsBanner posthog={posthog} setPreviousConsent={setPreviousConsent} setAskedForConsent={setAskedForConsent} />
+                </>
+            ) : null}
+            <PostHogProvider apiKey={process.env.NEXT_PUBLIC_POSTHOG_KEY}>
                 <GoogleAnalytics trackPageViews gaMeasurementId="GTM-5WPVB2J" />
+                <SessionProvider session={session} refetchInterval={5 * 58}>
+                    <NextNProgress color="#ffffff" />
+                    <Navbar />
+                    <div className="mx-auto px-4 sm:px-6 lg:px-8 pb-10 mt-5 ">
+                        <Component {...pageProps} />
 
-                <div className="mx-auto px-4 sm:px-6 lg:px-8 pb-10 mt-5 ">
-                    <Component {...pageProps} />
-
-                    {/* <Banner title="We are currently updating our infrastructure responsible for collecting metrics. This can cause instability on the stats page." /> */}
-                </div>
-            </SessionProvider>
+                        {/* <Banner title="We are currently updating our infrastructure responsible for collecting metrics. This can cause instability on the stats page." /> */}
+                    </div>
+                </SessionProvider>
+            </PostHogProvider>
         </>
     )
 }
