3b40c8512c7147fb396217ac61af15692ec8fdfb
Phillip Jensen <phillip@golemgrid.com>
2024-04-27 15:45:28 +0200

    Update search.mjs

--- markdoc/search.mjs
+++ markdoc/search.mjs
@@ -1,9 +1,9 @@
 import { createLoader } from "simple-functional-loader"
 import * as url from "url"
-
+import fetch from "node-fetch"
 
 const __filename = url.fileURLToPath(import.meta.url)
-import fetch from "node-fetch"
+
 export default function (nextConfig = {}) {
     return Object.assign({}, nextConfig, {
         webpack(config, options) {
@@ -15,55 +15,72 @@ export default function (nextConfig = {}) {
                             fetch(`${process.env.NEXT_PUBLIC_API_URL}v2/search-list`)
                                 .then((response) => response.json())
                                 .then(({ wallets, providers }) => {
+                                    // Combining wallets and providers into a single data array
                                     const data = [
                                         ...wallets.map((wallet) => ({
-                                            id: wallet,
+                                            provider_name: null,
+                                            url: `/network/providers/operator/${wallet}`,
                                             type: "wallet",
+                                            id: wallet,
                                         })),
                                         ...providers.map((provider) => ({
-                                            id: provider.id,
-                                            name: provider.provider_name,
+                                            provider_name: provider.provider_name,
+                                            url: `/network/provider/${provider.id}`,
                                             type: "provider",
+                                            id: provider.id,
                                         })),
                                     ]
 
                                     resolve(`
                                         import FlexSearch from 'flexsearch'
 
-                                        const index = new FlexSearch.Document({
-                                            tokenize: 'full',
-                                            document: {
-                                                id: 'id',
-                                                index: ['name', 'id'],
-                                                store: ['name', 'id', 'type'],
-                                            },
-                                            context: {
-                                                resolution: 9,
-                                                depth: 2,
-                                                bidirectional: true,
-                                            },
+                                        let sectionIndex = new FlexSearch.Document({
+                                          tokenize: 'full',
+                                            cache: true,
+                                          document: {
+                                            id: 'id',
+                                            index: ['provider_name', 'type', 'id'],
+                                            store: ['provider_name', 'type', 'id'],
+                                          },
+                                          context: {
+                                            resolution: 9,
+                                            depth: 2,
+                                            bidirectional: true
+                                          }
                                         })
 
-                                        const data = ${JSON.stringify(data)}
+                                        let indexData = ${JSON.stringify(data)}
+                                        
 
-                                        data.forEach((item) => {
-                                            index.add(item)
-                                        })
+                                        for (let { 
+                                            provider_name, 
+                                            url, 
+                                            type, 
+                                            id
+                                         } of indexData) {
+                                            sectionIndex.add({
+                                                id,
+                                                type,
+                                                provider_name
+                                            })
+                                        }
+                                        console.log('Search index loaded', sectionIndex)
 
                                         export function search(query, options = {}) {
-                                            let result = sectionIndex.search(query, {
-                                              ...options,
-                                              enrich: true,
-                                            })
-                                            console.log(result)
-                                            if (result.length === 0) {
-                                              return []
-                                            }
-                                            return result[0].result.map((item) => ({
-                                                ...item,
-                                                type: item.type,
-                                            }))
+                                          let result = sectionIndex.search(query, {
+                                            ...options,
+                                            enrich: true,
+                                          })
+                                          if (result.length === 0) {
+                                            return []
                                           }
+                                          return result[0].result.map((item) => ({
+                                             type: item.type,
+                                                provider_name: item.provider_name,
+                                                id: item.id,
+
+                                          }))
+                                        }
                                     `)
                                 })
                                 .catch((error) => {
