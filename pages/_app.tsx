c68ba59db3af596aae0496b9f817a6f9fd9a6937
Phillip Jensen <phillip@golemgrid.com>
2024-09-24 14:04:14 +0200

    Update _app.tsx

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -42,6 +42,7 @@ export default function App({ Component, pageProps: { session, ...pageProps } }:
                     }}
                 >
                     <Component {...pageProps} />
+                    <Banner title="We're performing live maintenance on our database. The stats page may not work as expected meanwhile." />
                 </div>
             </SessionProvider>
         </div>
