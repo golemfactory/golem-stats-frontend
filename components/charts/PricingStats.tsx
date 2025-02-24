f909efc25f24036df80cbc3c2fb6dbdf271d06eb
Phillip Jensen <phillip@golemgrid.com>
2025-02-24 11:25:02 +0100

    Update PricingStats.tsx

--- components/charts/PricingStats.tsx
+++ components/charts/PricingStats.tsx
@@ -7,6 +7,26 @@ import Skeleton from "react-loading-skeleton"
 import "react-loading-skeleton/dist/skeleton.css"
 import { MultiSelect, MultiSelectItem, SearchSelect, SearchSelectItem, SelectItem } from "@tremor/react"
 
+interface MetricCardSummaryProps {
+    unit: string;
+}
+
+interface TimeFrameFormatOptions {
+    month: "short";
+    day: "numeric";
+    year?: "numeric";
+}
+
+interface PricingDataItem {
+    date: number;
+    median_cpu: number;
+    average_cpu: number;
+    median_env: number;
+    average_env: number;
+    median_start: number;
+    average_start: number;
+}
+
 const PricingStats = () => {
     const [network, setNetwork] = useState("mainnet")
     const { data: metricData } = useSWR("v2/network/pricing/historical", fetcher, { refreshInterval: 60000 })
@@ -17,28 +37,25 @@ const PricingStats = () => {
     const [selectedMetric, setSelectedMetric] = useState("Median")
     const tabs = [{ name: "Median" }, { name: "Average" }]
 
-    const isDataValid = (data) => {
-        return !(isNaN(data) || data === null)
+    const isValidNumber = (value: number) => {
+        return typeof value === "number" && !isNaN(value)
     }
-
+    console.log(metricData)
     // Initialize values to a default (e.g., null or 0)
     let cpuValue = null
     let envValue = null
     let startValue = null
 
     // Check if data is available before trying to access its properties
-    if (data && metricData) {
-        const networkData = metricData[network] // Assuming metricData contains historical data as per your structure
-        const currentData = data // Assuming 'data' contains the current metric values
-
-        // Now, safely attempt to assign values if 'data' and 'metricData' are not null
-        // Adjust property access according to your actual data structure
-        cpuValue = selectedMetric === "Median" ? data[`${network}`].cpu_median : data[`${network}`].cpu_average
-        envValue = selectedMetric === "Median" ? data[`${network}`].env_median : data[`${network}`].env_average
-        startValue = selectedMetric === "Median" ? data[`${network}`].start_median : data[`${network}`].start_average
+    if (data && data[network] && metricData) {
+        const networkData = data[network]
+        cpuValue = selectedMetric === "Median" ? networkData.cpu_median : networkData.cpu_average
+        envValue = selectedMetric === "Median" ? networkData.env_median : networkData.env_average
+        startValue = selectedMetric === "Median" ? networkData.start_median : networkData.start_average
     }
 
-    if (!metricData || !data)
+    if (!metricData || !data) {
+        console.log("No data available", metricData, data)
         return (
             <Card>
                 <div className="relative px-6 mb-6">
@@ -54,70 +71,58 @@ const PricingStats = () => {
                 </div>
             </Card>
         )
-    const timeFrames = Object.keys(metricData[network])
-    const formatDate = (dateString, timeFrame) => {
+    }
+
+    const timeFrames = metricData && metricData[network] ? Object.keys(metricData[network]) : []
+    const formatDate = (dateString: number, timeFrame: string): string => {
         const date = new Date(dateString * 1000)
-        let formatOptions = {}
-        switch (timeFrame) {
-            case "7d":
-            case "1m":
-                formatOptions = { month: "short", day: "numeric" }
-                break
-            case "6m":
-            case "1y":
-            case "All":
-                formatOptions = { month: "short", day: "numeric", year: "numeric" }
-                break
+        let formatOptions: TimeFrameFormatOptions = {
+            month: "short",
+            day: "numeric"
+        }
+        
+        if (["6m", "1y", "All"].includes(timeFrame)) {
+            formatOptions.year = "numeric"
         }
         return date.toLocaleString("en-US", formatOptions)
     }
-    const MetricCardSummary = ({ unit }) => {
+
+    const MetricCardSummary = ({ unit }: MetricCardSummaryProps) => {
         return (
             <div className="flex justify-between">
                 <div>
                     <h3 className="text-tremor-default font-medium text-tremor-content dark:text-dark-tremor-content">The past hour</h3>
                     <div className="flex gap-4">
-                        {isDataValid(cpuValue) && (
-                            <div className="flex items-baseline space-x-2">
-                                <span className="text-tremor-metric font-semibold dark:text-dark-tremor-content-metric font-inter">
-                                    {RoundingFunction(cpuValue, 3)}
-                                </span>
-                                <span className="text-tremor-default font-medium text-tremor-brand-golemblue dark:text-dark-tremor-brand-golemblue">
-                                    CPU/h
-                                </span>
-                            </div>
-                        )}
-                        {isDataValid(envValue) && (
-                            <div className="flex items-baseline space-x-2">
-                                <span className="text-tremor-metric font-semibold dark:text-dark-tremor-content-metric font-inter">
-                                    {RoundingFunction(envValue, 3)}
-                                </span>
-                                <span className="text-tremor-default font-medium text-tremor-brand-golemblue dark:text-dark-tremor-brand-golemblue">
-                                    Env/h
-                                </span>
-                            </div>
-                        )}
-                        {isDataValid(startValue) && (
-                            <div className="flex items-baseline space-x-2">
-                                <span className="text-tremor-metric font-semibold dark:text-dark-tremor-content-metric font-inter">
-                                    {RoundingFunction(startValue, 3)}
-                                </span>
-                                <span className="text-tremor-default font-medium text-tremor-brand-golemblue dark:text-dark-tremor-brand-golemblue">
-                                    Start Price
-                                </span>
-                            </div>
-                        )}
+                        <div className="flex items-baseline space-x-2">
+                            <span className="text-tremor-metric font-semibold dark:text-dark-tremor-content-metric font-inter">
+                                {isValidNumber(cpuValue) ? RoundingFunction(cpuValue, 3) : "0"}
+                            </span>
+                            <span className="text-tremor-default font-medium text-tremor-brand-golemblue dark:text-dark-tremor-brand-golemblue">
+                                CPU/h
+                            </span>
+                        </div>
+                        <div className="flex items-baseline space-x-2">
+                            <span className="text-tremor-metric font-semibold dark:text-dark-tremor-content-metric font-inter">
+                                {isValidNumber(envValue) ? RoundingFunction(envValue, 3) : "0"}
+                            </span>
+                            <span className="text-tremor-default font-medium text-tremor-brand-golemblue dark:text-dark-tremor-brand-golemblue">
+                                Env/h
+                            </span>
+                        </div>
+                        <div className="flex items-baseline space-x-2">
+                            <span className="text-tremor-metric font-semibold dark:text-dark-tremor-content-metric font-inter">
+                                {isValidNumber(startValue) ? RoundingFunction(startValue, 3) : "0"}
+                            </span>
+                            <span className="text-tremor-default font-medium text-tremor-brand-golemblue dark:text-dark-tremor-brand-golemblue">
+                                Start Price
+                            </span>
+                        </div>
                     </div>
                 </div>
             </div>
         )
     }
-    //     <span className="text-tremor-metric font-medium">
-    //     Env: {RoundingFunction(data[`env_${unit.toLowerCase()}`], 3)}
-    // </span>
-    // <span className="text-tremor-metric font-medium">
-    //     Start: {RoundingFunction(data[`start_${unit.toLowerCase()}`], 3)}
-    // </span>
+
     return (
         <Card>
             <div className="flex flex-col md:flex-row justify-between items-start p-6">
@@ -150,10 +155,10 @@ const PricingStats = () => {
                 </TabList>
                 <TabPanels>
                     {tabs.map((tab, index) => (
-                        <TabPanel className="px-6 py-4">
+                        <TabPanel key={index} className="px-6 py-4">
                             <div className="grid md:flex md:items-start md:justify-between">
                                 <MetricCardSummary unit={selectedMetric} />
-                                <div className="order-1 md:order-2 pb-8  pt-4">
+                                <div className="order-1 md:order-2 pb-8 pt-4">
                                     <TabGroup>
                                         <TabList variant="solid" className="w-full md:w-fit">
                                             {timeFrames.map((frame) => (
@@ -170,18 +175,15 @@ const PricingStats = () => {
                                 </div>
                             </div>
                             <AreaChart
-                                data={(metricData[network][selectedTimeFrame] || []).map((item) => {
-                                    // Transforming the item
-                                    const transformedItem = {
+                                data={(metricData && metricData[network] && metricData[network][selectedTimeFrame] 
+                                    ? metricData[network][selectedTimeFrame].map((item: PricingDataItem) => ({
                                         ...item,
                                         date: formatDate(item.date, selectedTimeFrame),
                                         "CPU/h": selectedMetric === "Median" ? item.median_cpu : item.average_cpu,
                                         "Env/h": selectedMetric === "Median" ? item.median_env : item.average_env,
                                         "Start Price": selectedMetric === "Median" ? item.median_start : item.average_start,
-                                    }
-
-                                    return transformedItem
-                                })}
+                                    }))
+                                    : [])}
                                 autoMinValue={true}
                                 index="date"
                                 categories={["CPU/h", "Env/h", "Start Price"]}
